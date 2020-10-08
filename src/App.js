bfcc1d4d44389e0e461375255451ede906c2cbf4
Burhan GÖK <burhangok@yahoo.com>
2020-10-08 13:57:38 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-const API_URL = 'https://covid19.mathdro.id/api';
-const {countries = [],query,selectPlace} = this.props;
+import axios from 'axios';	
+const API_URL = 'https://covid19.mathdro.id/api/';
 
 class App extends Component {
   state = {
     countries: []
   }
   componentDidMount() {
-    const url = '${API_URL}/countries/';
+    const url = `${API_URL}/countries/`;
     axios.get(url).then(response => response.data)
     .then((data) => {
       this.setState({ countries: data })
       console.log(this.state.countries)
      })
   }
- render() {
+
+render() {
 
     return (
        <div className="container">
         <div className="col-xs-8">
-        <h1>Kamion React Project</h1>
+        <h1>Covid-19 React Project</h1>
+        {this.state.countries.map((country) => (
           <div className="card">
-            <div className="card-body">
-              <h3 className="card-title"> { this.state.countries.map(country => <li>{country.name}</li>)}</h3>
+           <div className="card-body">
+               <h3 className="card-title">{country.name}</h3>
+              
             </div>
           </div>
+        ))}
         </div>
        </div>
     );
   }
-}
+  }
+
 export default App;
